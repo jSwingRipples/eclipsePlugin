359b662090fa5ca5a929a217c77bcc68d1c96ce3
Cristian Guzmán <cristiangp91@gmail.com>
2016-05-28 16:08:19 -0300

    Functional version of Eclipse Plug-in

--- src/jrippleseclipseplugin/handlers/MenuButtonHandler.java
+++ src/jrippleseclipseplugin/handlers/MenuButtonHandler.java
@@ -1,11 +1,13 @@
 package jrippleseclipseplugin.handlers;
 
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -24,11 +26,26 @@ public class MenuButtonHandler extends AbstractHandler {
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"JRipplesEclipsePlugin",
-				"Holder of Places");
+		//IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		//MessageDialog.openInformation(
+		//		window.getShell(),
+		//		"JRipplesEclipsePlugin",
+		//		"Holder of Places");
+		
+		// Launching the JSwingRipples application
+		try {
+			String route = "C:/Users/Cristian/Workspace/JRipplesEclipse/eclipsePlugin/src/jrippleseclipseplugin/handlers/jswingripples.jar";
+			File file = new File(route);
+			URL[] urls = { file.toURI().toURL() };
+			URLClassLoader loader = new URLClassLoader(urls);
+			Class<?> cls = loader.loadClass("org.incha.ui.JSwingRipplesApplication");
+			Method main = cls.getDeclaredMethod("main", String[].class);
+			String[] args = {};
+			main.invoke(null, (Object)args);
+			loader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 }

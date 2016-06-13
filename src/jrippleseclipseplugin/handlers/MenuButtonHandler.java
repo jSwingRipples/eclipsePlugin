7078986aaa2068c0185174ef8f0b905099bede3b
Cristian Guzmán <cristiangp91@gmail.com>
2016-06-13 15:05:59 -0400

    Changes for Ripples to run correctly

--- src/jrippleseclipseplugin/handlers/MenuButtonHandler.java
+++ src/jrippleseclipseplugin/handlers/MenuButtonHandler.java
@@ -1,13 +1,8 @@
 package jrippleseclipseplugin.handlers;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+import org.incha.ui.JSwingRipplesApplication;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -25,30 +20,10 @@ public class MenuButtonHandler extends AbstractHandler {
 	 * the command has been executed, so extract extract the needed information
 	 * from the application context.
 	 */
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		//IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		//MessageDialog.openInformation(
-		//		window.getShell(),
-		//		"JRipplesEclipsePlugin",
-		//		"Holder of Places");
-		
-		// Launching the JSwingRipples application
-		try {
-
-	        URL location = MenuButtonHandler.class.getProtectionDomain().getCodeSource().getLocation();
-			String route = location.getFile()+"jswingripples.jar";
-			//System.out.println(route);
-			File file = new File(route);
-			URL[] urls = { file.toURI().toURL() };
-			URLClassLoader loader = new URLClassLoader(urls);
-			Class<?> cls = loader.loadClass("org.incha.ui.JSwingRipplesApplication");
-			Method main = cls.getDeclaredMethod("main", String[].class);
-			String[] args = {};
-			main.invoke(null, (Object)args);
-			loader.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public Object execute(ExecutionEvent event) {
+		String[] args = {};
+		JSwingRipplesApplication.main(args);
 		return null;
 	}
+	
 }
